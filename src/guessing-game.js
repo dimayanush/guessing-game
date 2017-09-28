33cc590b2884ad5333d88f799d0796f4e73bc5d7
Dmitry Yanushevich <yanushevich.1997@gmail.com>
2017-09-28 14:18:38 +0300

    completed task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+    	this.min = this.max = 0;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
 
+    	return  Math.round((this.max+this.min)/2);
     }
 
     lower() {
-
+    	this.max =  Math.round((this.max + this.min)/2);
     }
 
     greater() {
-
+    	this.min = Math.round((this.max + this.min)/2);
     }
 }
 
